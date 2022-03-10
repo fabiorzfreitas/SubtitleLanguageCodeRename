edb1e9b604d70e5d47fab85baf7731988379d3e9
fabiorzfreitas <mckfabio@gmail.com>
2022-03-10 12:47:00 -0300

    Back quote fix

--- Reverse.cmd
+++ Reverse.cmd
@@ -1,4 +1,4 @@
 @echo of
 chcp 65001
-for /f "usebackq delims=" %%g in ("dir *.srt /s /b") do ren "%%~fg" "%%~ng"
-for /f "usebackq delims=" %%g in ("dir *.pt-BR /s /b") do ren "%%~fg" "%%~ng.srt"
+for /f "usebackq delims=" %%g in (`dir *.srt /s /b`) do ren "%%~fg" "%%~ng"
+for /f "usebackq delims=" %%g in (`dir *.pt-BR /s /b`) do ren "%%~fg" "%%~ng.srt"
--- SubtitleLanguageCodeRename.cmd
+++ SubtitleLanguageCodeRename.cmd
@@ -1,3 +1,3 @@
 @echo off
 chcp 65001
-for /f "usebackq delims=" %%g in ("dir *.srt /s /b") do ren "%%~fg" "%%~ng.pt-BR.srt"
+for /f "usebackq delims=" %%g in (`dir *.srt /s /b`) do ren "%%~fg" "%%~ng.pt-BR.srt"
